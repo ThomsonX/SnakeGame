4925a83c122db70fc066eedb67ec6bce7c9541e0
SemkiShow <mgdeveloper123@gmail.com>
2026-05-20 17:18:43 +0200

    Tidy the code a bit

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,33 @@
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
 #include <raylib.h>
 #include <raymath.h>
 #include <vector>
-#include <ctime>
 
-int size = 50;
+const float CELL_SIZE = 50;
 
 Vector2 GetBoardSize()
 {
-    return {(float)floor(GetRenderWidth() / size), (float)floor(GetRenderHeight() / size)};
+    return {(float)floor(GetRenderWidth() / CELL_SIZE),
+            (float)floor(GetRenderHeight() / CELL_SIZE)};
 }
 
 struct Apple
 {
     Vector2 position{2, 2};
-    Texture2D apple = LoadTexture("resources/apple.png");
-    void RandomPlacement()
+    Texture2D texture = LoadTexture("resources/apple.png");
+
+    void SetRandomPosition()
     {
         Vector2 boardSize = GetBoardSize();
-        position = {(float)(rand() % (int)boardSize.x), (float)(rand() % (int)boardSize.y)};
+        position = {(float)GetRandomValue(0, boardSize.x), (float)GetRandomValue(0, boardSize.y)};
     }
     void Draw()
     {
-        
-       
-
-    DrawTexturePro(apple,{0, 0, (float)apple.width, (float)apple.height},{(float)position.x * size, (float)position.y * size, (float)size, (float)size},{0, 0},0,WHITE);
-        //DrawRectangle(position.x * size, position.y * size, size, size, RED);
+        DrawTexturePro(texture, {0, 0, (float)texture.width, (float)texture.height},
+                       {position.x * CELL_SIZE, position.y * CELL_SIZE, CELL_SIZE, CELL_SIZE},
+                       {0, 0}, 0, WHITE);
     }
 };
 
@@ -42,7 +44,7 @@ struct Snake
     std::vector<Vector2> body = {{2, 0}, {1, 0}, {0, 0}};
     Direction direction = Right;
 
-    void Move(bool removeLast)
+    void Move(bool removeLast = true)
     {
         Vector2 head = body[0];
         switch (direction)
@@ -62,40 +64,39 @@ struct Snake
         }
 
         Vector2 boardSize = GetBoardSize();
-        if (head.x < 0)
-            head.x = boardSize.x - 1;
-        if (head.x > boardSize.x - 1)
-            head.x = 0;
-        if (head.y < 0)
-            head.y = boardSize.y - 1;
-        if (head.y > boardSize.y - 1)
-            head.y = 0;
+        if (head.x < 0) head.x = boardSize.x - 1;
+        if (head.x > boardSize.x - 1) head.x = 0;
+        if (head.y < 0) head.y = boardSize.y - 1;
+        if (head.y > boardSize.y - 1) head.y = 0;
 
         body.insert(body.begin(), head);
-        if (removeLast)
-            body.pop_back();
+        if (removeLast) body.pop_back();
     }
 
     void HandleCollision(Apple& apple)
     {
         if (apple.position == body[0])
         {
-            apple.RandomPlacement();
+            apple.SetRandomPosition();
             Move(false);
         }
     }
 
     void Update()
     {
-        Move(true);
+        if (IsKeyPressed(KEY_LEFT) && direction != Right) direction = Left;
+        if (IsKeyPressed(KEY_RIGHT) && direction != Left) direction = Right;
+        if (IsKeyPressed(KEY_UP) && direction != Down) direction = Up;
+        if (IsKeyPressed(KEY_DOWN) && direction != Up) direction = Down;
     }
 
     void Draw()
     {
         for (int i = 0; i < body.size(); i++)
         {
-            // DrawTexturePro(Texture2D texture, Rectangle source, Rectangle dest, Vector2 origin, float rotation, WHITE);
-            DrawRectangle(body[i].x * size, body[i].y * size, size, size, ColorLerp(GREEN, WHITE, i * 1.0 / body.size()));
+            Color color = ColorLerp(GREEN, WHITE, i * 1.0 / body.size());
+            DrawRectangle(body[i].x * CELL_SIZE, body[i].y * CELL_SIZE, CELL_SIZE, CELL_SIZE,
+                          color);
         }
     }
 };
@@ -112,7 +113,7 @@ int main()
 
     Snake snake;
     Apple apple;
-    apple.RandomPlacement();
+    apple.SetRandomPosition();
 
     while (!WindowShouldClose())
     {
@@ -122,19 +123,12 @@ int main()
 
         if (GetTime() - timer >= tickTime)
         {
-            snake.Update();
+            snake.Move();
             snake.HandleCollision(apple);
             timer = GetTime();
         }
 
-        if (IsKeyPressed(KEY_LEFT) && snake.direction != Right)
-            snake.direction = Left;
-        if (IsKeyPressed(KEY_RIGHT) && snake.direction != Left)
-            snake.direction = Right;
-        if (IsKeyPressed(KEY_UP) && snake.direction != Down)
-            snake.direction = Up;
-        if (IsKeyPressed(KEY_DOWN) && snake.direction != Up)
-            snake.direction = Down;
+        snake.Update();
 
         snake.Draw();
         apple.Draw();
